29874634cfbc039e3d988023e257e515e0e4bf73
Phyo2000 <phyoaungzaymm@gmail.com>
2023-12-24 19:22:35 +0630

    SetUp

---
+++ lib/route/app_page.dart
@@ -0,0 +1,10 @@
+import 'package:ecommerce/route/app_route.dart';
+import 'package:ecommerce/view/dashboard/dashboard_screen.dart';
+import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'package:get/get.dart';
+
+class AppPage {
+  static var list = [
+    GetPage(name: AppRoute.dashboard, page: () => DashboardScreen())
+  ];
+}
