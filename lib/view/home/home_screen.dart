73b85abde78e2dd07e5923d25810c250b7050387
Phyo2000 <phyoaungzaymm@gmail.com>
2023-12-28 20:50:06 +0630

    Home Carousel Slider

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -1,5 +1,8 @@
 import 'package:ecommerce/component/main_header.dart';
+import 'package:ecommerce/controller/controllers.dart';
+import 'package:ecommerce/view/home/components/carousel_slider/carousel_loading.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,7 +12,14 @@ class HomeScreen extends StatelessWidget {
     return SafeArea(
       child: Column(
         children: [
-          MainHeader(),
+          const MainHeader(),
+          Obx(() {
+            if (homeController.isBannerLoading.value) {
+              return CarouselLoading();
+            } else {
+              return CarouselLoading();
+            }
+          }),
         ],
       ),
     );
