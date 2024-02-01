5a7413f17d7a6c6672bfabc30977fc43ba5bd8a1
Phyo2000 <phyoaungzaymm@gmail.com>
2024-02-01 10:19:53 +0630

    Popular category

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -1,15 +1,21 @@
 import 'package:ecommerce/model/ad_banner.dart';
+import 'package:ecommerce/model/category.dart';
 import 'package:ecommerce/service/remote_service/remote_banner_service.dart';
+import 'package:ecommerce/service/remote_service/remote_popular_category_service.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
   static HomeController instance = Get.find();
   RxList<AdBanner> bannerList = List<AdBanner>.empty(growable: true).obs;
+  RxList<Category> popularCategoryList =
+      List<Category>.empty(growable: true).obs;
   RxBool isBannerLoading = false.obs;
+  RxBool isPopularCategoryLoading = false.obs;
 
   @override
   void onInit() {
     getAdBanners();
+    getPopularCategories();
     super.onInit();
   }
 
@@ -25,6 +31,20 @@ class HomeController extends GetxController {
       // print("###########################");
       // print(bannerList.first.image);
       isBannerLoading(false);
+    }
+  }
+
+  void getPopularCategories() async {
+    try {
+      isPopularCategoryLoading(true);
+
+      var result = await RemotePopularCategoryService().get();
+      if (result != null) {
+        popularCategoryList.assignAll(popularCategoryListFromJson(result.body));
+      }
+    } finally {
+      print(popularCategoryList.length);
+      isPopularCategoryLoading(false);
     }
   }
 }
--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:ecommerce/component/main_header.dart';
 import 'package:ecommerce/controller/controllers.dart';
 import 'package:ecommerce/view/home/components/carousel_slider/carousel_loading.dart';
 import 'package:ecommerce/view/home/components/carousel_slider/carousel_slider_view.dart';
+import 'package:ecommerce/view/home/components/popular_category/popular_category_loading.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -16,10 +17,16 @@ class HomeScreen extends StatelessWidget {
           const MainHeader(),
           Obx(() {
             if (homeController.bannerList.isNotEmpty) {
-              print("here");
               return CarouselSliderView(bannerList: homeController.bannerList);
             } else {
               return CarouselLoading();
+            }
+          }),
+          Obx(() {
+            if (homeController.popularCategoryList.isNotEmpty) {
+              return const PopularCategoryLoading();
+            } else {
+              return PopularCategoryLoading();
             }
           }),
         ],
